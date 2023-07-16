a81891d7567f4aa27e9ab37a8462ea51ab4a40da
laugh4mile <laugh4mile@naver.com>
2023-07-16 22:42:58 +0900

    first commit

---
+++ src/main/java/com/example/dgdgbirthday/DgdgbirthdayApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgdgbirthday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgdgbirthdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgdgbirthdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dgdgbirthday/DgdgbirthdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgdgbirthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgdgbirthdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
